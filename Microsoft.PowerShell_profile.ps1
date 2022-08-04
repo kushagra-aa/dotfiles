22904d31719fe1d94ef0fa718a474e39cacffa1b
kushagra-aa <kushagraagnihotri24@gmial.com>
2022-08-04 09:28:38 +0530

    Initial commit

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,31 @@
+# Alias
+Set-Alias vim nvim
+Set-Alias ll ls
+Set-Alias g git
+Set-Alias g git
+
+# for Starship Promt
+Invoke-Expression (&starship init powershell)
+
+# for Terminal Icons
+Import-Module -Name Terminal-Icons
+
+# for Posh Git
+Import-Module posh-git
+
+Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
+
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -PredictionViewStyle ListView
+Set-PSReadLineOption -EditMode Windows
+
+Register-ArgumentCompleter -Native -CommandName winget -ScriptBlock {
+    param($wordToComplete, $commandAst, $cursorPosition)
+    [Console]::InputEncoding = [Console]::OutputEncoding = $OutputEncoding = [System.Text.Utf8Encoding]::new()
+    $Local:word = $wordToComplete.Replace('"', '""')
+    $Local:ast = $commandAst.ToString().Replace('"', '""')
+    winget complete --word="$Local:word" --commandline "$Local:ast" --position $cursorPosition | ForEach-Object {
+        [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+    }
+}
